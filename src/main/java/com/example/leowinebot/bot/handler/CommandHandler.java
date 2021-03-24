507490859f0579340362393baec96cdfac04f813
Viacheslav <viacheslav.semenyuk@gmail.com>
2021-03-24 17:35:24 +0200

    command fix

--- src/main/java/com/example/leowinebot/bot/handler/CommandHandler.java
+++ src/main/java/com/example/leowinebot/bot/handler/CommandHandler.java
@@ -20,6 +20,36 @@ public class CommandHandler {
     private UserService userService;
 
     @Autowired
+    private SearchHandler searchHandler;
+
+    @Autowired
+    private ProfileHandler profileHandler;
+
+    @Autowired
+    private ProfileEditHandler profileEditHandler;
+
+    @Autowired
+    private ProfileAboutEditHandler profileAboutEditHandler;
+
+    @Autowired
+    private ProfilePhotoEditHandler profilePhotoEditHandler;
+
+    @Autowired
+    private MatchHandler matchHandler;
+
+    @Autowired
+    private ActiveHandler activeHandler;
+
+    @Autowired
+    private AfterMatchHandler afterMatchHandler;
+
+    @Autowired
+    private StartHandler startHandler;
+
+    @Autowired
+    private MessageHandler messageHandler;
+
+    @Autowired
     private List<Handler> handlers;
 
 
@@ -39,10 +69,32 @@ public class CommandHandler {
             userService.save(user);
         }
 
-        handlers.stream()
-                .filter(handler -> handler.test(user.getUserStates()))
-                .findFirst()
-                .get().handle(message, user, chatId);
+        if (user.getUserStates().equals("search")) {
+            searchHandler.handle(message, user, chatId);
+        } else if (user.getUserStates().equals("profile")) {
+            profileHandler.handle(message, user, chatId);
+        } else if (user.getUserStates().equals("profileEdit")) {
+            profileEditHandler.handle(message, user, chatId);
+        } else if (user.getUserStates().equals("profilePhotoEdit")) {
+            profilePhotoEditHandler.handle(message, user, chatId);
+        } else if (user.getUserStates().equals("profileAboutEdit")) {
+            profileAboutEditHandler.handle(message, user, chatId);
+        } else if (user.getUserStates().equals("match")) {
+            matchHandler.handle(message, user, chatId);
+        } else if (user.getUserStates().equals("active")) {
+            activeHandler.handle(message, user, chatId);
+        } else if (user.getUserStates().equals("afterMatch")) {
+            afterMatchHandler.handle(message, user, chatId);
+        } else if (user.getUserStates().equals("message")) {
+            messageHandler.handle(message, user, chatId);
+        } else if (user.getUserStates().equals("start")) {
+            startHandler.handle(message, user, chatId);
+        }
+
+//        handlers.stream()
+//                .filter(handler -> handler.test(user.getUserStates()))
+//                .findFirst()
+//                .get().handle(message, user, chatId);
 
     }
 }
